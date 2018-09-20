30d8d2960dc04af9af46afc3805812313cb2787d
Royce Bradley <broyce18@yahoo.com>
2018-09-20 14:48:57 -0400

    Add files via upload

---
+++ comp2402a1/Part4.java
@@ -0,0 +1,72 @@
+package comp2402a1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class Part4 {
+	
+	/**
+	 * Your code goes here - see Part0 for an example
+	 * @param r the reader to read from
+	 * @param w the writer to write to
+	 * @throws IOException
+	 */
+	public static void doIt(BufferedReader r, PrintWriter w) throws IOException {
+		// Your code goes here - see Part0 for an example
+		String temp = "";
+		int count = 0;
+		ArrayList<String> strings = new ArrayList<String>();
+
+		for (String line = r.readLine(); line != null; line = r.readLine()) {
+			if(count <= 50) {
+				strings.add(line);
+				count++;
+				}
+				//Collections.reverse(strings);
+
+			if(count == 50) {
+				Collections.reverse(strings);
+				for (String text : strings) {
+					w.println(text);
+				}
+				strings.clear();
+				count = 0;
+			}
+		}
+	}
+
+	/**
+	 * The driver.  Open a BufferedReader and a PrintWriter, either from System.in
+	 * and System.out or from filenames specified on the command line, then call doIt.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			BufferedReader r;
+			PrintWriter w;
+			if (args.length == 0) {
+				r = new BufferedReader(new InputStreamReader(System.in));
+				w = new PrintWriter(System.out);
+			} else if (args.length == 1) {
+				r = new BufferedReader(new FileReader(args[0]));
+				w = new PrintWriter(System.out);				
+			} else {
+				r = new BufferedReader(new FileReader(args[0]));
+				w = new PrintWriter(new FileWriter(args[1]));
+			}
+			long start = System.nanoTime();
+			doIt(r, w);
+			w.flush();
+			long stop = System.nanoTime();
+			System.out.println("Execution time: " + 1e-9 * (stop-start));
+		} catch (IOException e) {
+			System.err.println(e);
+			System.exit(-1);
+		}
+	}
+}
